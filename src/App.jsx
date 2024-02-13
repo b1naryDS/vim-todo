ba3123cb2439d9423c53bebbb80500e3521259eb
Davor <davor.sehic@gmail.com>
2024-02-13 13:00:07 +0100

    fixed lagging index, updated design a bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 import NavInfo from './components/NavInfo';
 const App = () => {
   const [todos, setTodos] = useState([
-    { id: 1, text: 'Task 1', completed: false },
+    { id: 1, text: 'Task example writing something longer', completed: false },
     { id: 2, text: 'Task 2', completed: false },
     { id: 3, text: 'Task 3', completed: false },
     { id: 4, text: 'Task 4', completed: false },
@@ -13,8 +13,22 @@ const App = () => {
   ]);
 
   const [selectedTodoIndex, setSelectedTodoIndex] = useState(0);
+  const selectedTodoIndexRef = useRef(selectedTodoIndex);
+
   useEffect(() => {
+    selectedTodoIndexRef.current = selectedTodoIndex;
+  }, [selectedTodoIndex]);
+  useEffect(() => {
+    const handleSpace = (ind) => {
+      setTodos((prevTodos) => {
+        return prevTodos.map((todo, index) => {
+          return index === ind ? { ...todo, completed: !todo.completed } : todo;
+        });
+      });
+    };
+
     const handleKeyDown = (e) => {
+      const currentIndex = selectedTodoIndexRef.current;
       switch (e.key) {
         case 'j':
           setSelectedTodoIndex((prevIndex) =>
@@ -27,7 +41,7 @@ const App = () => {
           );
           break;
         case ' ':
-          handleSpace();
+          handleSpace(currentIndex);
         default:
           break;
       }
@@ -38,21 +52,7 @@ const App = () => {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [todos]);
-
-  useEffect(() => {
-    console.log(selectedTodoIndex);
-  }, [selectedTodoIndex]);
-
-  const handleSpace = () => {
-    setTodos((prevTodos) => {
-      return prevTodos.map((todo, index) => {
-        return index === selectedTodoIndex
-          ? { ...todo, completed: !todo.completed }
-          : todo;
-      });
-    });
-  };
+  }, []);
 
   const handleCheckboxChange = (index) => {
     const updatedTodos = [...todos];
@@ -66,29 +66,36 @@ const App = () => {
       <br />
       <div className='bg-red w-full'>
         {todos.map((todo, index) => (
-          <div className='w-full' key={todo.id}>
-            {selectedTodoIndex === index ? (
-              <span className='inline-block w-[13px]'>*</span>
-            ) : (
-              <span className='inline-block w-[13px]'></span>
-            )}
-            <input
-              className='w-[30px]'
-              type='checkbox'
-              checked={todo.completed}
-              onChange={() => handleCheckboxChange(index)}
-            />
-            <span
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none'
-              }}
-            >
-              {todo.text}
-            </span>
-          </div>
+          <Todo selected={selectedTodoIndex === index} key={todo.id}>
+            <>
+              <span className='inline-block w-[13px]'>
+                {selectedTodoIndex === index ? '*' : null}
+              </span>
+
+              <input
+                className='w-[30px] m-1'
+                type='checkbox'
+                checked={todo.completed}
+                onChange={() => handleCheckboxChange(index)}
+              />
+              <span
+                style={{
+                  textDecoration: todo.completed ? 'line-through' : 'none'
+                }}
+              >
+                {todo.text}
+              </span>
+            </>
+          </Todo>
         ))}
       </div>
     </div>
   );
+};
+
+const Todo = ({selected, children}) => {
+    const base = 'w-full my-1 flex flex-row gap-2 px-4 py-1';
+  const t = selected ? base + ' bg-[#9c52b3] text-white rounded-md shadow-md' : base + '';
+  return <div className={t}>{children}</div>;
 };
 export default App;
