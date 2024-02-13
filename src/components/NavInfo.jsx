1a7613c4a47b3ed9f59d444bf9658de957469127
Davor <davor.sehic@gmail.com>
2024-02-13 15:45:20 +0100

    added nav info more details

--- src/components/NavInfo.jsx
+++ src/components/NavInfo.jsx
@@ -1,8 +1,11 @@
+import { Timer } from "lucide-react";
+
 export default function NavInfo() {
 return (
       <><strong>J</strong> for down,<br/>
       <strong>K </strong>for up,<br/>
       <strong>Space</strong> for checking,<br/>
-      <strong>Enter</strong> for more info<br/>
-    <strong>N</strong> for new todo<br/></>)
+      <p className="flex items-center gap-2"><strong>Enter</strong> for more info<Timer className="inline mx-2" size="20" />(coming soon)<br/></p>
+      <p className="flex items-center gap-2"><strong>N</strong> for new todo<Timer className="inline mx-2" size="20" />(coming soon)<br/></p>
+    </>)
 }
